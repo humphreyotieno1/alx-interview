abaefba3d4f6f106d888f43d8345835c1443c027
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-15 01:01:50 +0300

    Pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains a function to generate Pascal's triangle.
-
-Pascal's triangle is a triangular array of binomial coefficients. Each number in the triangle is the sum of the two numbers directly above it.
-
-The function pascal_triangle(n) takes an integer n as input and returns a list of lists representing the Pascal's triangle up to the nth row.
-    [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+Pascal's Triangle
 """
 
 def pascal_triangle(n):
