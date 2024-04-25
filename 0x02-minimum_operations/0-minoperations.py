5ba7631e60c64b9527340b33f904124d297bdfe7
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-25 23:51:21 +0300

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''Minimum Operations'''
+
+
+def minOperations(n):
+    '''Function that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.'''
+    if n <= 1:
+        return 0
+
+    operations = 0
+    buffer = 1
+
+    while buffer < n:
+        if n % buffer == 0:
+            operations += n // buffer
+            buffer *= n // buffer
+            if buffer == n:
+                return operations
+        buffer += 1
+
+    return 0
