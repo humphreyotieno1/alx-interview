915af1527612a21539ec26872909cca8619ab8ea
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-26 00:03:15 +0300

    Minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,21 +2,15 @@
 '''Minimum Operations'''
 
 
-def minOperations(n):
-    '''Function that calculates the fewest number of operations needed to
-    result in exactly n H characters in the file.'''
-    if n <= 1:
-        return 0
+def minOperations(n: float) -> int:
+    '''function that returns the minimum operations needed'''
+    factors = []
+    div = 2
 
-    operations = 0
-    buffer = 1
-
-    while buffer < n:
-        if n % buffer == 0:
-            operations += n // buffer
-            buffer *= n // buffer
-            if buffer == n:
-                return operations
-        buffer += 1
-
-    return 0
+    while n > 1:
+        if n % div == 0:
+            factors.append(div)
+            n = n / div
+        else:
+            div += 1
+    return sum(factors)
