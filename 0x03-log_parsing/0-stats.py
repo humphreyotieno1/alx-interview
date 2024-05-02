f142ea1ff47401193402490706adc81d592dfad2
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-02 22:26:37 +0300

    Log parsing modification

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-log parsing
+Log parsing script
 """
 
 import sys
@@ -9,7 +9,8 @@ import re
 
 def output(log: dict) -> None:
     """
-    helper function to display stats
+    Helper function to display stats
+    :param log: Dictionary containing the log statistics
     """
     print("File size: {}".format(log["file_size"]))
     for code in sorted(log["code_frequency"]):
@@ -18,33 +19,35 @@ def output(log: dict) -> None:
 
 
 if __name__ == "__main__":
+    # Regular expression to match the log line format
     regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
 
     line_count = 0
     log = {}
     log["file_size"] = 0
     log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+        str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
 
     try:
         for line in sys.stdin:
             line = line.strip()
             match = regex.fullmatch(line)
-            if (match):
+            if match:
                 line_count += 1
                 code = match.group(1)
                 file_size = int(match.group(2))
 
-                # File size
+                # Update file size
                 log["file_size"] += file_size
 
-                # status code
-                if (code.isdecimal()):
+                # Update status code frequency
+                if code.isdecimal():
                     log["code_frequency"][code] += 1
 
-                if (line_count % 10 == 0):
+                # Print stats every 10 lines
+                if line_count % 10 == 0:
                     output(log)
     finally:
+        # Print final stats
         output(log)
