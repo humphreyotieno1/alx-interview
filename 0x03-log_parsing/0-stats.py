a1ae65921a6a7f4e9d4ad0d978a51033ed554fab
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-02 22:16:28 +0300

    Log parsing modification

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,63 +1,59 @@
-#!/usr/bin/env python3
-"""
-Log Parsing Script
-"""
-import re
+#!/usr/bin/python3
+"""strict to read stdn line by line and compute metrics"""
 import sys
-from signal import signal, SIGINT
+import re
+import signal
 
 
-file_size = 0
-status_codes = {
-    200: 0, 301: 0, 400: 0, 401: 0,
-    403: 0, 404: 0, 405: 0, 500: 0
-}
-line_count = 0
+def validate_line(line):
+    """validates the line using regex and returns status code and file size"""
+    pattern = (
+        r"(\d+\.\d+\.\d+\.\d+) - \[.*\] \"GET /projects/260 HTTP/1\.1\" "
+        r"(\d+) (\d+)"
+    )
+    matching = re.match(pattern, line)
+    if matching:
+        status_code = matching.group(2)
+        file_size = int(matching.group(3))
+        return [status_code, file_size]
+    return None
 
 
-def print_stats(signal_received=None, frame=None):
-    """
-    Print the summarized log statistics.
-    """
-    print("File size: {}".format(file_size))
-    sorted_codes = sorted(status_codes.keys())
-    for code in sorted_codes:
-        if status_codes[code]:
-            print("{}: {}".format(code, status_codes[code]))
+def print_metrics(status, f_size):
+    """prints the status code and filesize metrics"""
+    print(f"File size: {f_size}")
+    for key, value in status.items():
+        if value > 0:
+            print(f"{key}: {value}")
 
 
-signal(SIGINT, print_stats)
+def signal_handler(sig, frame):
+    """handles CTRL-C interrupt signal and prints metrics before exit"""
+    print_metrics(status_codes, total_file_size)
+    sys.exit(0)
 
 
-def parse_line(line):
-    """
-    Parse a single log line and update the metrics.
-    """
-    global file_size, line_count
-    line_pattern = r'^(\d+.\d+.\d+.\d+)' \
-                   r' - \[(.+?)\]' \
-                   r' "GET \/projects\/260 HTTP\/1.1" (\d+) (\d+)'
-    match = re.match(line_pattern, line)
-    if not match:
-        return
-
-    ip_address, date, status_code, size = match.groups()
+def main():
+    """main entry point of the program"""
+    line_done = 0
+    global status_codes, total_file_size
+    status_codes = {
+        "200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0
+        }
+    total_file_size = 0
+    signal.signal(signal.SIGINT, signal_handler)
     try:
-        status_code = int(status_code)
-    except ValueError:
-        return
-
-    file_size += int(size)
-    status_codes[status_code] += 1
-    line_count += 1
-
-    if line_count % 10 == 0:
-        print_stats()
+        for line in sys.stdin:
+            result = validate_line(line)
+            total_file_size += result[1]
+            status_codes[result[0]] += 1
+            line_done += 1
+            if line_done % 10 == 0:
+                print_metrics(status_codes, total_file_size)
+    except KeyboardInterrupt:
+        print_metrics(status_codes, total_file_size)
 
 
 if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            parse_line(line.rstrip())
-    except KeyboardInterrupt:
-        print_stats()
+    main()
