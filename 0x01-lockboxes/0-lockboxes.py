995be29f5a41c3eeacd6e026fe8fbf65173cf937
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-04 11:01:34 +0300

    Lockboxes implementation

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+def canUnlockAll(boxes):
+    # Create a set to keep track of the boxes that have been visited
+    visited = set()
+
+    # Add the first box to the visited set
+    visited.add(0)
+
+    # Create a stack to store the boxes to be visited
+    stack = [0]
+
+    # Iterate through the stack until it is empty
+    while stack:
+        # Pop the top box from the stack
+        current_box = stack.pop()
+
+        # Iterate through the keys in the current box
+        for key in boxes[current_box]:
+            # If the key opens a new box and the box has not been visited yet
+            if key < len(boxes) and key not in visited:
+                # Add the box to the visited set
+                visited.add(key)
+
+                # Add the box to the stack to be visited later
+                stack.append(key)
+
+    # If all boxes have been visited, return True
+    return len(visited) == len(boxes)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,2 @@
+The lockboxes algorithm is a popular problem-solving technique that involves unlocking a series of lockboxes. Each lockbox contains a key that can be used to unlock another lockbox. The goal is to determine if it is possible to unlock all the lockboxes.
+
