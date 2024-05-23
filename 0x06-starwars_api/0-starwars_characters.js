b967ed0bf374bd40ea6234e9d29ae811567249c3
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-23 23:43:53 +0300

    Star wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,35 +2,39 @@
 
 const request = require('request');
 
-if (process.argv.length < 3) {
-    console.log('Usage: ./0-starwars_characters.js <movie_id>');
-    process.exit(1);
+if (process.argv.length !== 3) {
+  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
 }
 
 const movieId = process.argv[2];
-const apiURL = `https://swapi.dev/api/films/${movieId}/`;
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(apiURL, (error, response, body) => {
-    if (error) {
-        console.error(error);
-        process.exit(1);
-    }
-    if (response.statusCode !== 200) {
-        console.error(`Error ${response.statusCode} - ${response.statusMessage}`);
-        process.exit(1);
-    }
+request(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    const movie = JSON.parse(body);
-    const characters = movie.characters;
+  if (res.statusCode !== 200) {
+    console.log(`Error: ${res.statusCode}`);
+    return;
+  }
 
-    characters.forEach((charactersUrl) => {
-        request(charactersUrl, (error, res, messBody) => {
-            if (error) {
-                console.error(error);
-            } else {
-                const character = JSON.parse(messBody);
-                console.log(character.name);
-            }
-        });
+  const characters = body.characters;
+  characters.forEach(characterUrl => {
+    request(characterUrl, { json: true }, (err, res, body) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      if (res.statusCode !== 200) {
+        console.log(`Error: ${res.statusCode}`);
+        return;
+      }
+
+      console.log(body.name);
     });
+  });
 });
