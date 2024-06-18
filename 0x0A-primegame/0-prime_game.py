7c9c64a746c865784c05c843043bb1df0a381f2b
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-06-18 13:56:55 +0300

    Prime game alorithm

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
-'''Prime game algorithm'''
-
-
-def sieve(n):
-    '''Generate prime numbers'''
-    prime = [True for number in range(n + 1)]
-    start_prime = 2
-
-    while (start_prime * start_prime <= n):
-        if prime[start_prime]:
-            for num in range(start_prime * start_prime, n + 1, start_prime):
-                prime[num] = False
-        start_prime += 1
-
-    list_prime = [i for i in range(2, n+1) if prime[i]]
-    return list_prime
+"""Prime game algorithm"""
 
 
 def isWinner(x, nums):
-    '''Function to determine winner'''
-    max_to_reach = max(nums)
-    primes = sieve(max_to_reach)
-
-    prime_count = [0] * (max_to_reach + 1)
-    for prime in primes:
-        for i in range(prime, max_to_reach + 1, prime):
-            prime_count[i] += 1
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if prime_count[n] % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """Function to etermine the winner"""
+    if x < 1 or not nums:
         return None
+    marias_wins, bens_wins = 0, 0
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
